fa3aba04d8a38e3c70081f360b6ce1b6a3a41725
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-07-28 11:23:48 -0300

    Commit del día 28-07-2023

--- PMF.py
+++ PMF.py
@@ -1,8 +1,14 @@
 from PyPDF2 import PdfFileMerger
+import os
+from pathlib import Path
 
-ruta=r''
 
-pdfs=[ruta+'.pdf', ruta+'.pdf']
+ruta=r'C:/Users/jbaut/Desktop/Programing/Python/Udemy-Python Developer/Proyectos/PDF-Merger-Python/pdf-merger-python'
+
+os.path.print
+
+
+pdfs=[ruta+'/27-07-2023 Factura Vicario.pdf', ruta+'/27-07-2023 Factura Vega.pdf']
 
 merger=PdfFileMerger()
 
